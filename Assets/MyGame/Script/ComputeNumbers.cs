793717f114e9ed07e772719dd53088e4d71c3b21
sresl <sarah.resl21@gmail.com>
2019-12-17 15:53:29 +0100

    finished project of adding numbers

---
+++ Assets/MyGame/Script/ComputeNumbers.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ComputeNumbers : MonoBehaviour
+{ 
+    public TextMeshProUGUI result;
+
+    public TMP_InputField varA;
+    public TMP_InputField varB;
+
+    public Button btn_reset;
+    public Button btn_add_Numbers;
+
+
+public void SetResult()
+{
+    result.text = AddNumbers().ToString();
+
+        varA.interactable = false;
+        varB.interactable = false;
+
+            btn_add_Numbers.interactable = false;
+            btn_reset.interactable = true;
+}
+
+public void Reset()
+{
+    varA.text = "";
+    varB.text = "";
+
+        varA.interactable = true;
+        varB.interactable = true;
+
+           btn_add_Numbers.interactable = true;
+           btn_reset.interactable = false;
+
+    result.text = "Result";
+}
+
+private float AddNumbers()
+{
+    float tempResult = float.Parse(varA.text) + float.Parse(varB.text);
+    
+    return tempResult;
+}
+}
